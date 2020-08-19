5420b4d31ee8e2ae8dfc2c01b4b84df57211fe67
Nicolas Medvedy <nicolasivanm@gmail.com>
2020-08-18 23:27:05 -0300

    slider implemented

--- slider.js
+++ slider.js
@@ -1,5 +1,20 @@
-//$(document).ready(function(){
-   // $('.s2').slick({
-    //  autoplay: true,
-   // });
- // }); 
+$('.owl-carousel').owlCarousel({
+  loop:false,
+  margin:10,
+  nav:false,  
+  autoWidth:false,  
+  responsiveClass:true,
+  
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  },  
+}) 
+
